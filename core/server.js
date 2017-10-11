8faf7530dd2ff55118781aa46cd3867f5c31ab6f
newton <newton@newtonflash.com>
2017-10-11 13:00:56 +0530

    moved server.js to core directory for more clean code.

--- core/server.js
+++ core/server.js
@@ -3,27 +3,29 @@ var express         = require('express');
 var bodyParser      = require('body-parser');
 var app             = express();
 var fs 				= require('fs');
-var siteConfig 		= require('./core/site-config.js');
+var siteConfig 		= require('./site-config.js');
 
 var serveIndex      = require('serve-index');
 var exphbs          = require('express-handlebars');
 var favicon         = require('serve-favicon');
 
+const PUBLIC_DIRECTORY = "../public";
 
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, PUBLIC_DIRECTORY)));
 //app.use(express.static(path.join(__dirname, 'views/services')));
-app.use('/images',express.static(path.join(__dirname, 'public/images')));
-app.use('/js',express.static(path.join(__dirname, 'public/js')));
-app.use('/css',express.static(path.join(__dirname, 'public/css')));
-app.use('/components', serveIndex('views/components/', {'icons': true}));
+app.use('/images',express.static(path.join(__dirname, PUBLIC_DIRECTORY + '/images')));
+app.use('/js',express.static(path.join(__dirname, PUBLIC_DIRECTORY + '/js')));
+app.use('/css',express.static(path.join(__dirname, PUBLIC_DIRECTORY + '/css')));
+app.use('/components', serveIndex('../views/components/', {'icons': true}));
 app.use('/js', serveIndex('public/js/', {'icons': true}));
+app.use('/css', serveIndex('public/css/', {'icons': true}));
 
-app.use(favicon(path.join(__dirname, 'public/images', 'favicon.png')));
+app.use(favicon(path.join(__dirname, PUBLIC_DIRECTORY + '/images', 'favicon.png')));
 
 app.engine('handlebars', exphbs({
     defaultLayout: 'main',
@@ -35,7 +37,7 @@ app.engine('handlebars', exphbs({
 app.set('view engine', siteConfig.templateEngine);
 
 
-require("./core/routes")(app, siteConfig, express);
+require("./routes")(app, siteConfig, express);
 
 var server = app.listen(siteConfig.portNo, function () {
     var host = server.address().address;
